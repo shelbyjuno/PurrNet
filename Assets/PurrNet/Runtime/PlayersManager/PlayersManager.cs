a35f41dd2618852fac19d2b754735a65d7d4dd66
Valentin Bordeianu <blenminer123@gmail.com>
2024-06-30 11:53:11 +0200

    sync players done

--- Assets/PurrNet/Runtime/PlayersManager/PlayersManager.cs
+++ Assets/PurrNet/Runtime/PlayersManager/PlayersManager.cs
@@ -8,28 +8,54 @@ namespace PurrNet.Modules
 {
     public partial struct ClientLoginRequest : IAutoNetworkedData
     {
-        public string join;
+        public string join { get; }
+        
+        public ClientLoginRequest(string join)
+        {
+            this.join = join;
+        }
     }
     
     public partial struct ServerLoginResponse : IAutoNetworkedData
     {
-        public PlayerID playerId { get; set; }
+        public PlayerID playerId { get; }
+
+        public ServerLoginResponse(PlayerID playerId)
+        {
+            this.playerId = playerId;
+        }
     }
     
     public partial struct PlayerJoinedEvent : IAutoNetworkedData
     {
-        public PlayerID playerId;
-        public Connection connection;
+        public PlayerID playerId { get; }
+        public Connection connection { get; }
+        
+        public PlayerJoinedEvent(PlayerID playerId, Connection connection)
+        {
+            this.playerId = playerId;
+            this.connection = connection;
+        }
     }
     
     public partial struct PlayerLeftEvent : IAutoNetworkedData
     {
-        public PlayerID playerId;
+        public PlayerID playerId { get; }
+        
+        public PlayerLeftEvent(PlayerID playerId)
+        {
+            this.playerId = playerId;
+        }
     }
     
     public partial struct PlayerSnapshotEvent : IAutoNetworkedData
     {
-        public Dictionary<Connection, PlayerID> _connectionToPlayerId { get; set; }
+        public Dictionary<Connection, PlayerID> _connectionToPlayerId { get; }
+        
+        public PlayerSnapshotEvent(Dictionary<Connection, PlayerID> connectionToPlayerId)
+        {
+            _connectionToPlayerId = connectionToPlayerId;
+        }
     }
     
     public class PlayersManager : INetworkModule, IConnectionListener
@@ -42,11 +68,14 @@ namespace PurrNet.Modules
         private uint _playerIdCounter;
         
         private readonly Dictionary<Connection, PlayerID> _connectionToPlayerId = new();
+        private readonly Dictionary<PlayerID, Connection> _playerToConnection = new();
         private readonly List<PlayerID> _connectedPlayers = new();
         private PlayerID? _localPlayerId;
         
         public List<PlayerID> connectedPlayers => _connectedPlayers;
         public PlayerID? localPlayerId => _localPlayerId;
+
+        private bool _asServer;
 
         public PlayersManager(NetworkManager nm, CookiesModule cookiesModule, BroadcastModule broadcaste)
         {
@@ -62,6 +91,8 @@ namespace PurrNet.Modules
         
         public void Enable(bool asServer)
         {
+            _asServer = asServer;
+            
             if (asServer)
             {
                 _broadcastModule.RegisterCallback<ClientLoginRequest>(OnClientLoginRequest, true);
@@ -69,9 +100,28 @@ namespace PurrNet.Modules
             else
             {
                 _broadcastModule.RegisterCallback<ServerLoginResponse>(OnClientLoginResponse, false);
+                _broadcastModule.RegisterCallback<PlayerSnapshotEvent>(OnPlayerSnapshotEvent, false);
+                _broadcastModule.RegisterCallback<PlayerJoinedEvent>(OnPlayerJoinedEvent, false);
+                _broadcastModule.RegisterCallback<PlayerLeftEvent>(OnPlayerLeftEvent, false);
             }
         }
-        
+
+        private void OnPlayerJoinedEvent(Connection conn, PlayerJoinedEvent data, bool asserver)
+        {
+            RegisterPlayer(data.connection, data.playerId);
+        }
+
+        private void OnPlayerLeftEvent(Connection conn, PlayerLeftEvent data, bool asserver)
+        {
+            UnregisterPlayer(data.playerId);
+        }
+
+        private void OnPlayerSnapshotEvent(Connection conn, PlayerSnapshotEvent data, bool asserver)
+        {
+            foreach (var (key, pid) in _connectionToPlayerId)
+                RegisterPlayer(key, pid);
+        }
+
         private void OnClientLoginResponse(Connection conn, ServerLoginResponse data, bool asServer)
         {
             _localPlayerId = data.playerId;
@@ -81,7 +131,7 @@ namespace PurrNet.Modules
         {
             if (!_cookieToPlayerId.TryGetValue(data.join, out var playerId))
             {
-                playerId = new PlayerID(++_playerIdCounter);
+                playerId = new PlayerID(++_playerIdCounter, false);
                 _cookieToPlayerId.Add(data.join, playerId);
             }
             
@@ -93,26 +143,55 @@ namespace PurrNet.Modules
                 return;
             }
             
+            _broadcastModule.SendToClient(conn, new ServerLoginResponse(playerId));
+
+            SendSnapshotToClient(conn);
             RegisterPlayer(conn, playerId);
-            
-            _broadcastModule.SendToClient(conn, new ServerLoginResponse
-            {
-                playerId = playerId
-            }, Channel.ReliableUnordered);
+            SendNewUserToAllClients(conn, playerId);
+        }
+        
+        private void SendNewUserToAllClients(Connection conn, PlayerID playerId)
+        {
+            _broadcastModule.SendToAll(new PlayerJoinedEvent(playerId, conn));
+        }
+        
+        private void SendUserLeftToAllClients(PlayerID playerId)
+        {
+            _broadcastModule.SendToAll(new PlayerLeftEvent(playerId));
+        }
+        
+        private void SendSnapshotToClient(Connection conn)
+        {
+            _broadcastModule.SendToClient(conn, new PlayerSnapshotEvent(_connectionToPlayerId));
         }
 
         private void RegisterPlayer(Connection conn, PlayerID player)
         {
             _connectedPlayers.Add(player);
-            _connectionToPlayerId.Add(conn, player);
+
+            if (conn.isValid)
+            {
+                _connectionToPlayerId.Add(conn, player);
+                _playerToConnection.Add(player, conn);
+            }
         }
         
         private void UnregisterPlayer(Connection conn)
         {
-            if (!_connectionToPlayerId.TryGetValue(conn, out var player)) return;
+            if (!_connectionToPlayerId.TryGetValue(conn, out var player))
+                return;
             
             _connectedPlayers.Remove(player);
+            _playerToConnection.Remove(player);
             _connectionToPlayerId.Remove(conn);
+        }
+        
+        private void UnregisterPlayer(PlayerID playerId)
+        {
+            if (_playerToConnection.TryGetValue(playerId, out var conn))
+                _connectionToPlayerId.Remove(conn);
+            _connectedPlayers.Remove(playerId);
+            _playerToConnection.Remove(playerId);
         }
 
         public void Disable(bool asServer)
@@ -133,16 +212,16 @@ namespace PurrNet.Modules
             
             // Generate a new session cookie or get the existing one and send it to the server
             var cookie = _cookiesModule.GetOrSet("client_connection_session", Guid.NewGuid().ToString(), false);
-            _broadcastModule.SendToServer(new ClientLoginRequest
-            {
-                join = cookie
-            }, Channel.ReliableUnordered);
+            _broadcastModule.SendToServer(new ClientLoginRequest(cookie), Channel.ReliableUnordered);
         }
 
         public void OnDisconnected(Connection conn, bool asServer)
         {
             if (!asServer) return;
             
+            if (_connectionToPlayerId.TryGetValue(conn, out var playerId))
+                SendUserLeftToAllClients(playerId);
+
             UnregisterPlayer(conn);
         }
     }
