101b87497ce9432441842692d576e54d6aa866d4
Valentin BORDEIANU <blenminer123@gmail.com>
2024-08-27 15:44:08 +0200

    mzde network behaviour empty class abstrzct cause fuck yoiu

--- Assets/PurrNet/Runtime/NetworkBehaviour/NetworkBehaviour.cs
+++ Assets/PurrNet/Runtime/NetworkBehaviour/NetworkBehaviour.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 namespace PurrNet
 {
     [Icon("Assets/PurrNet/Editor/Editor Default Resources/cat-face-color-icon.png")]
-    public class NetworkBehaviour : NetworkIdentity { }
+    public abstract class NetworkBehaviour : NetworkIdentity { }
 }
