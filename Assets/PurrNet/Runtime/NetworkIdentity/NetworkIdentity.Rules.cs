61621e30f199c069a457e0d7fe75b7c65713e90b
Valentin Bordeianu <blenminer123@gmail.com>
2024-09-06 14:57:01 +0200

    visibility rules set override

--- Assets/PurrNet/Runtime/NetworkIdentity/NetworkIdentity.Rules.cs
+++ Assets/PurrNet/Runtime/NetworkIdentity/NetworkIdentity.Rules.cs
@@ -6,94 +6,97 @@ namespace PurrNet
     {
         [SerializeField, HideInInspector] private NetworkRules _networkRules;
         [SerializeField, HideInInspector] private NetworkVisibilityRuleSet _visitiblityRules;
+
+        private NetworkRules networkRules => _networkRules ? _networkRules : networkManager.networkRules;
         
-        protected NetworkRules networkRules => _networkRules ? networkManager.networkRules : _networkRules;
+        public NetworkVisibilityRuleSet visibilityRules => _visitiblityRules ? _visitiblityRules : networkManager.visibilityRules;
         
         public bool HasDespawnAuthority(PlayerID player, bool asServer)
         {
-            return networkRules.HasDespawnAuthority(this, player, asServer);
+            return networkRules && networkRules.HasDespawnAuthority(this, player, asServer);
         }
         
         public bool HasSpawnAuthority(NetworkManager manager, bool asServer)
         {
             var rules = _networkRules ? manager.networkRules : _networkRules;
-            return rules.HasSpawnAuthority(manager, asServer);
+            
+            return rules && rules.HasSpawnAuthority(manager, asServer);
         }
         
         public bool HasSetActiveAuthority(PlayerID player, bool asServer)
         {
-            return networkRules.HasSetActiveAuthority(this, player, asServer);
+            return networkRules && networkRules.HasSetActiveAuthority(this, player, asServer);
         }
         
         public bool HasSetActiveAuthority(bool asServer)
         {
-            return networkRules.HasSetActiveAuthority(this, localPlayer, asServer);
+            return networkRules && networkRules.HasSetActiveAuthority(this, localPlayer, asServer);
         }
         
         public bool HasSetEnabledAuthority(PlayerID player, bool asServer)
         {
-            return networkRules.HasSetEnabledAuthority(this, player, asServer);
+            return networkRules && networkRules.HasSetEnabledAuthority(this, player, asServer);
         }
         
         public bool HasSetEnabledAuthority(bool asServer)
         {
-            return networkRules.HasSetEnabledAuthority(this, localPlayer, asServer);
+            return networkRules && networkRules.HasSetEnabledAuthority(this, localPlayer, asServer);
         }
 
         public bool ShouldSyncParent(bool asServer)
         {
-            return networkRules.ShouldSyncParent(this, asServer);
+            return networkRules && networkRules.ShouldSyncParent(this, asServer);
         }
         
         public bool ShouldSyncSetActive(bool asServer)
         {
-            return networkRules.ShouldSyncSetActive(this, asServer);
+            return networkRules && networkRules.ShouldSyncSetActive(this, asServer);
         }
         
         public bool ShouldSyncSetEnabled(bool asServer)
         {
-            return networkRules.ShouldSyncSetEnabled(this, asServer);
+            return networkRules && networkRules.ShouldSyncSetEnabled(this, asServer);
         }
         
         public bool ShouldPropagateToChildren(bool asServer)
         {
-            return networkRules.ShouldPropagateToChildren(this, asServer);
+            return networkRules && networkRules.ShouldPropagateToChildren(this, asServer);
         }
         
         public bool ShouldOverrideExistingOwnership(bool asServer)
         {
-            return networkRules.ShouldOverrideExistingOwnership(this, asServer);
+            return networkRules && networkRules.ShouldOverrideExistingOwnership(this, asServer);
         }
         
         public bool HasPropagateOwnershipAuthority(bool asServer)
         {
-            return networkRules.HasPropagateOwnershipAuthority(this, asServer);
+            return networkRules && networkRules.HasPropagateOwnershipAuthority(this, asServer);
         }
         
         public bool HasChangeParentAuthority(bool asServer)
         {
-            return networkRules.HasChangeParentAuthority(this, localPlayer, asServer);
+            return networkRules && networkRules.HasChangeParentAuthority(this, localPlayer, asServer);
         }
         
         public bool HasChangeParentAuthority(PlayerID player, bool asServer)
         {
-            return networkRules.HasChangeParentAuthority(this, player, asServer);
+            return networkRules && networkRules.HasChangeParentAuthority(this, player, asServer);
         }
 
 
         public bool HasTransferOwnershipAuthority(bool asServer)
         {
-            return networkRules.HasTransferOwnershipAuthority(this, localPlayer, asServer);
+            return networkRules && networkRules.HasTransferOwnershipAuthority(this, localPlayer, asServer);
         }
         
         public bool HasTransferOwnershipAuthority(PlayerID player, bool asServer)
         {
-            return networkRules.HasTransferOwnershipAuthority(this, player, asServer);
+            return networkRules && networkRules.HasTransferOwnershipAuthority(this, player, asServer);
         }
 
         public bool HasGiveOwnershipAuthority(bool asServer)
         {
-            return networkRules.HasGiveOwnershipAuthority(this, asServer);
+            return networkRules && networkRules.HasGiveOwnershipAuthority(this, asServer);
         }
     }
 }
