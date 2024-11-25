de5b045c1d2adc5dd79d0702bdde8fe27d9209d6
BlenMiner <blenminer123@gmail.com>
2024-11-25 22:47:03 +0100

    testing sending packer directly

--- Assets/Examples/Reddit/SomeBehaviour.cs
+++ Assets/Examples/Reddit/SomeBehaviour.cs
@@ -1,9 +1,6 @@
-using System.Collections.Generic;
 using PurrNet;
 using PurrNet.Logging;
 using PurrNet.Packing;
-using PurrNet.StateMachine;
-using PurrNet.Transports;
 
 public class SomeBehaviour : NetworkBehaviour
 {
@@ -13,8 +10,10 @@ public class SomeBehaviour : NetworkBehaviour
         {
             using var writer = BitPackerPool.Get();
             
-            Packer<string>.Write(writer, "Hello, server!");
-            Packer<string>.Write(writer, "Maybe some audio data being pumped?");
+            Packer<byte>.Write(writer, 100);
+            
+            for (int i = 0; i < 100; i++)
+                Packer<string>.Write(writer, i.ToString());
 
             Stream(writer);
         }
@@ -25,14 +24,16 @@ public class SomeBehaviour : NetworkBehaviour
     {
         using (data)
         {
-            string message = default;
-            string audioData = default;
-
-            Packer<string>.Read(data, ref message);
-            Packer<string>.Read(data, ref audioData);
-
-            PurrLogger.Log(message); // Hello, server!
-            PurrLogger.Log(audioData); // Maybe some audio data being pumped?
+            byte count = 0;
+            
+            Packer<byte>.Read(data, ref count);
+            
+            for (int i = 0; i < count; i++)
+            {
+                string message = string.Empty;
+                Packer<string>.Read(data, ref message);
+                PurrLogger.Log(message); // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ...
+            }
         }
     }
 }
