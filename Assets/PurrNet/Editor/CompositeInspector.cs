846015364c5ff9e1d22e674a4a643713e43ad740
BobsiUnity <martin.skovrup@gmail.com>
2024-11-26 19:41:46 +0100

    Fixed composite transport stuff

--- Assets/PurrNet/Editor/CompositeInspector.cs
+++ Assets/PurrNet/Editor/CompositeInspector.cs
@@ -16,6 +16,8 @@ namespace PurrNet.Editor
 
         public override void OnInspectorGUI()
         {
+            serializedObject.Update();
+            
             var composite = (CompositeTransport)target;
             if (composite.clientState != ConnectionState.Disconnected || composite.listenerState != ConnectionState.Disconnected)
                 GUI.enabled = false;
@@ -23,6 +25,7 @@ namespace PurrNet.Editor
             GUI.enabled = true;
             
             TransportInspector.DrawTransportStatus(composite);
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
