7114e211942955576e334b24c48e0e326158e39c
BlenMiner <blenminer123@gmail.com>
2024-11-27 02:26:30 +0100

    put code in namespace

--- Assets/PurrNet/Editor/StateMachineEditor.cs
+++ Assets/PurrNet/Editor/StateMachineEditor.cs
@@ -1,153 +1,159 @@
-using PurrNet.StateMachine;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(StateMachine))]
-public class StateMachineEditor : Editor
+namespace PurrNet.StateMachine.InspectorEditor
 {
-    private StateMachine _stateMachine;
-    SerializedProperty _statesProperty;
-
-    private void OnEnable()
+    [CustomEditor(typeof(StateMachine))]
+    public class StateMachineEditor : UnityEditor.Editor
     {
-        _stateMachine = target as StateMachine;
-        _statesProperty = serializedObject.FindProperty("_states");
-    }
+        private StateMachine _stateMachine;
+        SerializedProperty _statesProperty;
 
-    public override void OnInspectorGUI()
-    {
-        serializedObject.Update();
-
-        EditorGUI.BeginChangeCheck();
-        if(Application.isPlaying)
-            EditorGUI.BeginDisabledGroup(true);
-        
-        EditorGUILayout.PropertyField(_statesProperty, new GUIContent("States"), true);
-    
-        if(Application.isPlaying)
-            EditorGUI.EndDisabledGroup();
-        
-        if(EditorGUI.EndChangeCheck())
+        private void OnEnable()
         {
-            EditorUtility.SetDirty(target);
-            serializedObject.ApplyModifiedProperties();
+            _stateMachine = target as StateMachine;
+            _statesProperty = serializedObject.FindProperty("_states");
+        }
+
+        public override void OnInspectorGUI()
+        {
             serializedObject.Update();
-        }
 
-        if (!Application.isPlaying)
-        {
-            EditorGUILayout.HelpBox("State information available during Play Mode", MessageType.Info);
-            return;
-        }
+            EditorGUI.BeginChangeCheck();
+            if (Application.isPlaying)
+                EditorGUI.BeginDisabledGroup(true);
 
-        DrawStateMachineInfo();
-        DrawStateControls();
-        DrawNetworkStatus();
-    
-        serializedObject.ApplyModifiedProperties();
-    }
+            EditorGUILayout.PropertyField(_statesProperty, new GUIContent("States"), true);
 
-    private void DrawStateMachineInfo()
-    {
-        EditorGUILayout.Space();
-        using(new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
-        {
-            EditorGUILayout.LabelField("State Machine Status", EditorStyles.boldLabel);
-        
-            // Current State
-            var currentState = _stateMachine.currentState;
-            EditorGUILayout.LabelField("Current State:", currentState.stateId >= 0 ? 
-                _stateMachine.states[currentState.stateId].GetType().Name : "None");
+            if (Application.isPlaying)
+                EditorGUI.EndDisabledGroup();
 
-            // Draw state data if exists
-            if (currentState.data != null)
+            if (EditorGUI.EndChangeCheck())
             {
-                EditorGUI.indentLevel++;
-                using(new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+                EditorUtility.SetDirty(target);
+                serializedObject.ApplyModifiedProperties();
+                serializedObject.Update();
+            }
+
+            if (!Application.isPlaying)
+            {
+                EditorGUILayout.HelpBox("State information available during Play Mode", MessageType.Info);
+                return;
+            }
+
+            DrawStateMachineInfo();
+            DrawStateControls();
+            DrawNetworkStatus();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawStateMachineInfo()
+        {
+            EditorGUILayout.Space();
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                EditorGUILayout.LabelField("State Machine Status", EditorStyles.boldLabel);
+
+                // Current State
+                var currentState = _stateMachine.currentState;
+                EditorGUILayout.LabelField("Current State:",
+                    currentState.stateId >= 0 ? _stateMachine.states[currentState.stateId].GetType().Name : "None");
+
+                // Draw state data if exists
+                if (currentState.data != null)
                 {
-                    EditorGUILayout.LabelField("State Data:", EditorStyles.boldLabel);
-                    DrawReadOnlyObject(currentState.data);
+                    EditorGUI.indentLevel++;
+                    using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+                    {
+                        EditorGUILayout.LabelField("State Data:", EditorStyles.boldLabel);
+                        DrawReadOnlyObject(currentState.data);
+                    }
+
+                    EditorGUI.indentLevel--;
                 }
-                EditorGUI.indentLevel--;
-            }
 
-            // Previous/Next State preview
-            if (currentState.stateId >= 0)
-            {
-                // Previous state - use actual previous state
-                string previousState = "None";
-                if (_stateMachine.previousStateId >= 0 && _stateMachine.previousStateId < _stateMachine.states.Count)
-                    previousState = _stateMachine.states[_stateMachine.previousStateId].GetType().Name;
-                EditorGUILayout.LabelField("Previous State:", previousState);
-
-                // Next state
-                var nextIdx = currentState.stateId < _stateMachine.states.Count - 1 ? currentState.stateId + 1 : 0;
-                EditorGUILayout.LabelField("Next State:", _stateMachine.states[nextIdx].GetType().Name);
-            }
-        }
-    }
-
-    private void DrawStateControls()
-    {
-        if (!Application.isPlaying || !_stateMachine.isServer)
-            return;
-
-        EditorGUILayout.Space();
-        using(new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
-        {
-            EditorGUILayout.LabelField("State Controls", EditorStyles.boldLabel);
-
-            using(new EditorGUILayout.HorizontalScope())
-            {
-                if (GUILayout.Button("Previous"))
-                    _stateMachine.Previous();
-
-                if (GUILayout.Button("Next"))
-                    _stateMachine.Next();
-            }
-        }
-    }
-
-    private void DrawNetworkStatus()
-    {
-        EditorGUILayout.Space();
-        using(new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
-        {
-            EditorGUILayout.LabelField("Network Status", EditorStyles.boldLabel);
-            EditorGUILayout.LabelField("Role:", _stateMachine.isServer ? "Server" : "Client");
-        }
-    }
-
-    private void DrawReadOnlyObject(object obj)
-    {
-        if (obj == null) return;
-
-        if (obj is Object unityObj)
-        {
-            var serializedObject = new SerializedObject(unityObj);
-            var iterator = serializedObject.GetIterator();
-            bool enterChildren = true;
-        
-            while (iterator.NextVisible(enterChildren))
-            {
-                using(new EditorGUI.DisabledScope(true))
+                // Previous/Next State preview
+                if (currentState.stateId >= 0)
                 {
-                    EditorGUILayout.PropertyField(iterator, true);
+                    // Previous state - use actual previous state
+                    string previousState = "None";
+                    if (_stateMachine.previousStateId >= 0 &&
+                        _stateMachine.previousStateId < _stateMachine.states.Count)
+                        previousState = _stateMachine.states[_stateMachine.previousStateId].GetType().Name;
+                    EditorGUILayout.LabelField("Previous State:", previousState);
+
+                    // Next state
+                    var nextIdx = currentState.stateId < _stateMachine.states.Count - 1 ? currentState.stateId + 1 : 0;
+                    EditorGUILayout.LabelField("Next State:", _stateMachine.states[nextIdx].GetType().Name);
                 }
-                enterChildren = false;
             }
         }
-        else
+
+        private void DrawStateControls()
         {
-            if (obj.GetType().IsPrimitive || obj is string)
+            if (!Application.isPlaying || !_stateMachine.isServer)
+                return;
+
+            EditorGUILayout.Space();
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
             {
-                EditorGUILayout.LabelField("Value", obj.ToString());
+                EditorGUILayout.LabelField("State Controls", EditorStyles.boldLabel);
+
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    if (GUILayout.Button("Previous"))
+                        _stateMachine.Previous();
+
+                    if (GUILayout.Button("Next"))
+                        _stateMachine.Next();
+                }
+            }
+        }
+
+        private void DrawNetworkStatus()
+        {
+            EditorGUILayout.Space();
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                EditorGUILayout.LabelField("Network Status", EditorStyles.boldLabel);
+                EditorGUILayout.LabelField("Role:", _stateMachine.isServer ? "Server" : "Client");
+            }
+        }
+
+        private void DrawReadOnlyObject(object obj)
+        {
+            if (obj == null) return;
+
+            if (obj is Object unityObj)
+            {
+                var serializedObject = new SerializedObject(unityObj);
+                var iterator = serializedObject.GetIterator();
+                bool enterChildren = true;
+
+                while (iterator.NextVisible(enterChildren))
+                {
+                    using (new EditorGUI.DisabledScope(true))
+                    {
+                        EditorGUILayout.PropertyField(iterator, true);
+                    }
+
+                    enterChildren = false;
+                }
             }
             else
             {
-                foreach (var field in obj.GetType().GetFields(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public))
+                if (obj.GetType().IsPrimitive || obj is string)
                 {
-                    EditorGUILayout.LabelField(field.Name, field.GetValue(obj)?.ToString() ?? "null");
+                    EditorGUILayout.LabelField("Value", obj.ToString());
+                }
+                else
+                {
+                    foreach (var field in obj.GetType().GetFields(System.Reflection.BindingFlags.Instance |
+                                                                  System.Reflection.BindingFlags.Public))
+                    {
+                        EditorGUILayout.LabelField(field.Name, field.GetValue(obj)?.ToString() ?? "null");
+                    }
                 }
             }
         }
