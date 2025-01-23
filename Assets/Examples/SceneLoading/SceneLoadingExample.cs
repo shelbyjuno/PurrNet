9683593fd6f0294dee7147cef6be52b40edb60d5
BlenMiner <blenminer123@gmail.com>
2025-01-23 21:49:21 +0100

    random tests

--- Assets/Examples/SceneLoading/SceneLoadingExample.cs
+++ Assets/Examples/SceneLoading/SceneLoadingExample.cs
@@ -1,5 +1,5 @@
-using System;
 using PurrNet;
+using PurrNet.Logging;
 using PurrNet.Modules;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -11,14 +11,14 @@ public class SceneLoadingExample : MonoBehaviour
     [SerializeField] private bool isPubic;
     [SerializeField] private LoadSceneMode loadSceneMode;
 
-    [ContextMenu("Add All")]
+    [ContextMenu("Tests/Add All")]
     public void AddAllPlayers()
     {
         var scenes = NetworkManager.main.GetModule<ScenesModule>(true);
         var scenesPlayers = NetworkManager.main.GetModule<ScenePlayersModule>(true);
         var players = NetworkManager.main.GetModule<PlayersManager>(true);
 
-        var scene = SceneManager.GetSceneByBuildIndex(sceneIndex);
+        var scene = gameObject.scene;
         if (scenes.TryGetSceneID(scene, out var sceneID))
         {
             for (var i = 0; i < players.players.Count; i++)
@@ -27,9 +27,28 @@ public class SceneLoadingExample : MonoBehaviour
                 scenesPlayers.AddPlayerToScene(player, sceneID);
             }
         }
+        else PurrLogger.LogError($"Scene with build index {sceneIndex} '{scene.name}' not found");
     }
     
-    [ContextMenu("Execute")]
+    [ContextMenu("Tests/Move All Players")]
+    public void MoveAllPlayers()
+    {
+        var scenes = NetworkManager.main.GetModule<ScenesModule>(true);
+        var scenesPlayers = NetworkManager.main.GetModule<ScenePlayersModule>(true);
+        var players = NetworkManager.main.GetModule<PlayersManager>(true);
+
+        var scene = gameObject.scene;
+        if (scenes.TryGetSceneID(scene, out var sceneID))
+        {
+            for (var i = 0; i < players.players.Count; i++)
+            {
+                var player = players.players[i];
+                scenesPlayers.MovePlayerToSingleScene(player, sceneID);
+            }
+        }
+    }
+    
+    [ContextMenu("Tests/Execute")]
     public void Execute()
     {
         var scenes = NetworkManager.main.GetModule<ScenesModule>(true);
